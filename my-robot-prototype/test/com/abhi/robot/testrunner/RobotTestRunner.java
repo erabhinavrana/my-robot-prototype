d20b2a071402054048acbee4d832612680fb2097
Abhinav <erabhinav.rana@gmail.com>
2016-09-25 14:11:17 +0400

    change to maven project

--- my-robot-prototype/test/com/abhi/robot/testrunner/RobotTestRunner.java
+++ my-robot-prototype/test/com/abhi/robot/testrunner/RobotTestRunner.java
@@ -3,6 +3,8 @@
  */
 package com.abhi.robot.testrunner;
 
+import java.util.List;
+
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
@@ -15,10 +17,9 @@ import com.abhi.robot.testsuite.RobotTestSuites;
 public class RobotTestRunner {
 	public static void main(String[] args) {
 	      Result result = JUnitCore.runClasses(RobotTestSuites.class);
-			
-	      for (Failure failure : result.getFailures()) {
-	         System.out.println(failure.toString());
-	      }
+		
+	      List<Failure> failureList = result.getFailures();
+	      failureList.forEach(failure->System.out.println(failure.toString()));
 			
 	      System.out.println("Robot Test Suites success :: " + result.wasSuccessful());
 	   }
